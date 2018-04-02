f5b22ef80a5c26b5a34ff6e52ee2f255ecf6420b
nev <snevedaren@gmail.com>
2018-04-03 00:25:30 +0700

    hello world

---
+++ server.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+from http.server import BaseHTTPRequestHandler,HTTPServer
+from os import curdir, sep
+
+PORT_NUMBER = 8080
+
+static_path = "static"
+
+class myHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path=="/":
+            self.path="/index.html"
+
+        try:
+            sendReply = False
+            if self.path.endswith(".html"):
+                mimetype='text/html'
+                sendReply = True
+            if self.path.endswith(".jpg"):
+                mimetype='image/jpg'
+                sendReply = True
+            if self.path.endswith(".js"):
+                mimetype='application/javascript'
+                sendReply = True
+            if self.path.endswith(".css"):
+                mimetype='text/css'
+                sendReply = True
+
+            if sendReply == True:
+                #Open the static file requested and send it
+                f = open(curdir + sep + static_path + sep + self.path, 'rb') 
+                self.send_response(200)
+                self.send_header('Content-type',mimetype)
+                self.end_headers()
+                self.wfile.write(f.read())
+                f.close()
+            return
+
+        except IOError:
+            self.send_error(404,'File Not Found: %s' % self.path)
+
+try:
+    #Create a web server and define the handler to manage the
+    #incoming request
+    server = HTTPServer(('', PORT_NUMBER), myHandler)
+    print ('Started httpserver on port' , PORT_NUMBER)
+    
+    #Wait forever for incoming htto requests
+    server.serve_forever()
+
+except KeyboardInterrupt:
+    print ('^C received, shutting down the web server')
+    server.socket.close()
