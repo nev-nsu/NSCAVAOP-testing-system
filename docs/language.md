a18ff27421d3660e9b21783c3c1bec04618d195f
nev <snevedaren@gmail.com>
2018-05-11 05:47:56 +0700

    All types of tests realization - Some changes in the language - Add messages to generate execptions - real, string, array, composite, choice, const types of tests added

--- docs/language.md
+++ docs/language.md
@@ -1,21 +1,21 @@
 **number** - целое число
+**real_number** - нецелое число
 **string** - строка
 **symbol** - один символ
 **identifier** - строка, связываемая как имя с соответствующим объектом
-**identifier** - строка, соответствующая существующему идентификатору
 
-**syntax** = «**number** : » **number** «; » {**define**} **test**
-**define** = «**define** » **identifier** «(» [**identifier** {«, » **identifier**}] «) as » **test**
+**syntax** = «number: » **number** «; » {**define**} **test**
+**define** = «define » **identifier** «(» [**identifier** {«, » **identifier**}] «) as » **test**
 **parameter** = **number** | **real **| **string** | **test** | **identifier**
 **define**d = **identifier** « { » [**identifier** « : » **parameter** {«; » **identifier** « : » **parameter**}] «}»
 **test** = **integer** | **real **| **string** | **array** | **composite** | **choice** | **const** | **identifier** | **define**d
-**numeric_type** = **number** | **identifier**
-**real_type** = **real **| **identifier**
-**integer** = «**integer** { min : » **numeric_type** «; max : » **numeric_type** [«; distribution : » (... | …)] [«; name : « **identifier**] « }»
-**real **= «**real **{ min : » **real_type** «; max : » **real_type** [«; distribution : » (... | …)] [«; name : » **identifier**] « }»
-**string** = «**string** { length : » (**integer** | **real_type**) [(«; allowed **symbol**s : » **symbol** {«,» **symbol**}) | («; forbidden **symbol**s : » **symbol** {«,» **symbol**})] [«; name : » **identifier**] « }»
-**array** = «**array** { length : » (**integer** | **numeric_type**) «; type : » **test** [«; name : » **identifier**] « }»
-**composite** = «**composite** { » **test** {«; » **test**} « }»
-**choice** = «**choice** { » **test** «; » **test** {«; » **test**} « }»
-**const** = «**const** { values :» **string** {«, » **string**} [«; name : » **identifier**] « }»
+**numeric_type** = **number** | **identifier** | **integer**
+**real_type** = **real** | **identifier** | **real_number**
+**integer** = «integer { min : » **numeric_type** «; max : » **numeric_type** [«; distribution : » (... | …)] [«; name : « **identifier**] « }»
+**real **= «real { min : » **real_type** «; max : » **real_type** [«; distribution : » (... | …)] [«; name : » **identifier**] « }»
+**string** = «string { length : » **numeric_type** [(«; allowed: » **symbol** {«,» **symbol**}) | («; forbidden: » **symbol** {«,» **symbol**})] [«; name : » **identifier**] « }»
+**array** = «**array** { length : » **numeric_type** «; element_type : » **test** [«; name : » **identifier**] « }»
+**composite** = «composite { » **test** {«; » **test**} « }»
+**choice** = «choice { » **test** «; » **test** {«; » **test**} « }»
+**const** = «const { value :» **string** [«; name : » **identifier**] « }»
 
--- kernel/generate.py
+++ kernel/generate.py
@@ -1,16 +1,36 @@
-class VariableRedefinition(Exception):
-    pass
-
 import random
+import string
+
+class VariableRedefinition(Exception):
+    def __init__(self, name):
+        message = "Variable redefinition: '" + name + "'"
+        super().__init__(message)
 
 class UnresolvedVariableName(Exception):
-    pass
+    def __init__(self, name):
+        message = "Unknown variable: '" + name + "'"
+        super().__init__(message)
 
 class BadTemplate(Exception):
-    pass
+    def __init__(self):
+        message = "Bad template"
+        super().__init__(message)
 
 class UnknownType(Exception):
-    pass
+    def __init__(self, name):
+        message = "Unknown type: '" + name + "'"
+        super().__init__(message)
+
+# Use it!
+class BadParameterType(Exception):
+    def __init__(self):
+        message = "Bad template: wrong parameter type"
+        super().__init__(message)
+
+class MissingParameter(Exception):
+    def __init__(self, name):
+        message = "Bad template: not optional parameter '" + name + "' is missing"
+        super().__init__(message)
 
 class Generator:
     def __init__(self, tests):
@@ -24,7 +44,6 @@ class Generator:
         except (AttributeError, KeyError):
             raise BadTemplate()
                 
-
     def __generate_instance__(self, template):
         self.names = {}
         return self.__generate_recursive__(template)['representation']
@@ -34,31 +53,72 @@ class Generator:
             attr = obj[name]
         else:
             if not optional:
-                raise BadTemplate()
+                raise MissingParameter(name)
             return None
         if attr['type'] == 'value':
             return attr['value']
         elif attr['type'] == 'variable':
             if not attr['name'] in self.names:
-                raise UnresolvedVariableName()
+                raise UnresolvedVariableName(attr['name'])
             return self.names[attr['name']]
         elif attr['type'] == 'test':
-            return __generate_recursive__(attr['template'])['raw']
+            return self.__generate_recursive__(attr['template'])['raw']
         else:
-            raise UnknownType()
+            raise UnknownType(attr['type'])
 
     def __generate_recursive__(self, template):
         name = self.__get_attribute__(template, 'name', True)
-        if self.__get_attribute__(template, 'type') == 'integer':
+        type = self.__get_attribute__(template, 'type')
+        if type == 'integer':
             max = self.__get_attribute__(template, 'max')
             min = self.__get_attribute__(template, 'min')
             num = random.randint(min, max)
             result = { 'raw': num, 'representation': str(num) }
+        elif type == 'real':
+            max = self.__get_attribute__(template, 'max')
+            min = self.__get_attribute__(template, 'min')
+            num = random.uniform(min, max)
+            result = { 'raw': num, 'representation': str(num) }
+        elif type == 'string':
+            length = self.__get_attribute__(template, 'length')
+            allowed = self.__get_attribute__(template, 'allowed', True)
+            forbidden = self.__get_attribute__(template, 'forbidden', True)
+            if allowed:
+                s = ''.join(random.choice(allowed) for _ in range(length))
+            elif forbidden:
+                s = ''.join(random.choice((string.ascii_uppercase + string.digits).translate(None, forbidden)) for _ in range(length))
+            else:
+                raise MissingParameter('allowed/forbidden')
+            result = { 'raw': s, 'representation': s }
+        elif type == 'array':
+            length = self.__get_attribute__(template, 'length')
+            inner = self.__get_attribute__(template, 'element_type')
+            a = [self.__generate_recursive__(inner) for _ in range(length)]
+            repr = ''.join([i['representation'] for i in a])
+            a = [i['raw'] for i in a]
+            result = { 'raw': a, 'representation': repr }
+        elif type == 'composite':
+            array = self.__get_attribute__(template, 'array')
+            if len(array) < 1:
+                raise BadTemplate()
+            a = [self.__generate_recursive__(x) for x in array]
+            repr = ''.join([i['representation'] for i in a])
+            a = [i['raw'] for i in a]
+            result = { 'raw': a, 'representation': repr }
+        elif type == 'choice':
+            array = self.__get_attribute__(template, 'array')
+            if len(array) < 1:
+                raise BadTemplate()
+            inner = random.choice(array)
+            result = self.__generate_recursive__(inner)
+        elif type == 'const':
+            value = self.__get_attribute__(template, 'value')
+            result = { 'raw': value, 'representation': value }
         else:
-            raise UnknownType()
+            raise UnknownType(type)
         if name:
             if name in self.names:
-                raise VariableRedefinition()
+                raise VariableRedefinition(name)
             self.names[name] = result['raw']
         return result
             
